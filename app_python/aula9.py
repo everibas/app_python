9a925db4af43470dba872eba73f51f2a0169f17c
Evelynne Ribas <ribas.analista@gmail.com>
2021-09-24 19:23:15 +0100

    Add files via upload

--- app_python/aula9.py
+++ app_python/aula9.py
@@ -1,5 +1,24 @@
-arquivo = open('teste.txt', 'a')
 # usar w para criar novo arquivo ou a para adicionar ao
 # fim do arquivo
-arquivo.write('Minha primeira escrita\n')
-arquivo.close()
+def escrever_arquivo(texto):
+    diretorio = 'C:/Projetos/globallabs/app_python/teste.txt'
+    arquivo = open(diretorio, 'w')
+    # arquivo = open('teste.txt', 'w')
+    arquivo.write(texto)
+    arquivo.close()
+
+def atualizar_arquivo(texto):
+
+    arquivo = open('teste.txt', 'a')
+    arquivo.write(texto)
+    arquivo.close()
+
+def ler_arquivo(nome_arquivo):
+    arquivo = open(nome_arquivo, 'r')
+    texto = arquivo.read()
+    print(texto)
+
+if __name__ == '__main__':
+    escrever_arquivo('Primeira linha.\n')
+   # atualizar_arquivo('Segunda linha.\n')
+   #ler_arquivo('teste.txt')
